582180c5c6c78f5a572cde334e9a283525476f35
jonas747 <jonasr747@gmail.com>
2016-01-30 21:48:55 +0100

    Fixed normalized vector2

--- Assets/Scripts/PlayerSpellControls.cs
+++ Assets/Scripts/PlayerSpellControls.cs
@@ -5,7 +5,7 @@ public class PlayerSpellControls : MonoBehaviour{
 	public int activeSpell; // an index for spells
 
 	void Start(){
-		spells = SpellManager.Instance.spells;
+		//spells = SpellManager.Instance.spells;
 	}
 
 	void Update(){
@@ -23,7 +23,8 @@ public class PlayerSpellControls : MonoBehaviour{
         Vector3 myPos = new Vector2(transform.position.x, transform.position.y);
         Vector3 direction = target - myPos;
         direction.Normalize();
-        GetComponent<Unit>().CastSpell(spells[activeSpell], direction.normalized);
-        Debug.Log(direction.normalized);
+        Vector2 normalized = (Vector2)direction;
+        normalized = normalized / normalized.magnitude;
+        GetComponent<Unit>().CastSpell(spells[activeSpell], normalized);
 	}
 }
