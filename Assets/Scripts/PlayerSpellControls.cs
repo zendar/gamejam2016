9d7cf9dfb1e2735403f24449c4b2a0e3309e5030
Kristian Kasin Nordlie <kristian@bitsquad-games.com>
2016-01-30 21:46:52 +0100

    Shotrate

--- Assets/Scripts/PlayerSpellControls.cs
+++ Assets/Scripts/PlayerSpellControls.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerSpellControls : MonoBehaviour{
 	public SpellType[] spells;
+    public float shotRate;
 	public int activeSpell; // an index for spells
 
+    private float shotCounter;
 	void Start(){
 		spells = SpellManager.Instance.spells;
+        shotCounter = shotRate;
 	}
 
 	void Update(){
-		if(Input.GetButtonDown("Fire1")){
+        shotCounter -= Time.deltaTime;
+
+		if(Input.GetButtonDown("Fire1") && shotCounter <= 0){
 			CastSpell();
+            shotCounter = shotRate;
 		}
 	}
 
