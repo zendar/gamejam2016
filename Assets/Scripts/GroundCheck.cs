352ff8bcb9f98eec3c97f3935afe6ba499cfbd55
Ricki Sickenger <ricki.sickenger@gmail.com>
2016-01-31 13:49:11 +0100

    Changed groundcheck to use radius of circlecollider

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -10,9 +10,12 @@ public class GroundCheck : MonoBehaviour{
 	}
     void FixedUpdate()
     {
-        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
-        Vector2 firstPoint = new Vector2((transform.position.x-(collider.size.x/2)*transform.localScale.x)+0.05f, transform.position.y - (collider.size.y/2*transform.localScale.y)-0.05f);
-        Vector2 secorndPoint = new Vector2(transform.position.x + collider.size.x/2*transform.localScale.x-0.05f , transform.position.y - (collider.size.y/2*transform.localScale.y)-0.5f);
+        CircleCollider2D collider = gameObject.GetComponent<CircleCollider2D>();
+		Vector2 firstPoint = new Vector2((transform.position.x-(collider.radius)*transform.localScale.x)+0.05f, transform.position.y - (collider.radius*transform.localScale.y)-0.28f);
+		Vector2 secorndPoint = new Vector2(transform.position.x + collider.radius*transform.localScale.x-0.05f , transform.position.y - (collider.radius*transform.localScale.y)-0.6f);
+
+
+//		Debug.DrawLine(firstPoint,secorndPoint);
 
         if (_playerControls != null)
         {
