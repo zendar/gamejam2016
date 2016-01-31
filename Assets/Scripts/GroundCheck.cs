b30bca5d2df111ad7b7fa08ef86e466e4a87ef98
Ricki Sickenger <ricki.sickenger@gmail.com>
2016-01-31 14:02:08 +0100

    Wops.. fixed groundcheck for ai.

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -10,15 +10,15 @@ public class GroundCheck : MonoBehaviour{
 	}
     void FixedUpdate()
     {
-        CircleCollider2D collider = gameObject.GetComponent<CircleCollider2D>();
-		Vector2 firstPoint = new Vector2((transform.position.x-(collider.radius)*transform.localScale.x)+0.05f, transform.position.y - (collider.radius*transform.localScale.y)-0.28f);
-		Vector2 secorndPoint = new Vector2(transform.position.x + collider.radius*transform.localScale.x-0.05f , transform.position.y - (collider.radius*transform.localScale.y)-0.6f);
-
 
 //		Debug.DrawLine(firstPoint,secorndPoint);
 
         if (_playerControls != null)
         {
+			CircleCollider2D collider = gameObject.GetComponent<CircleCollider2D>();
+		Vector2 firstPoint = new Vector2((transform.position.x-(collider.radius)*transform.localScale.x)+0.05f, transform.position.y - (collider.radius*transform.localScale.y)-0.28f);
+		Vector2 secorndPoint = new Vector2(transform.position.x + collider.radius*transform.localScale.x-0.05f , transform.position.y - (collider.radius*transform.localScale.y)-0.6f);
+
             if (Physics2D.OverlapArea(firstPoint, secorndPoint))
             {
                 _playerControls.grounded = true;
@@ -30,6 +30,11 @@ public class GroundCheck : MonoBehaviour{
         }
         if (_ai != null)
         {
+			BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+	        Vector2 firstPoint = new Vector2((transform.position.x-(collider.size.x/2)*transform.localScale.x)+0.05f, transform.position.y - (collider.size.y/2*transform.localScale.y)-0.05f);
+	        Vector2 secorndPoint = new Vector2(transform.position.x + collider.size.x/2*transform.localScale.x-0.05f , transform.position.y - (collider.size.y/2*transform.localScale.y)-0.5f);
+
+
             if (Physics2D.OverlapArea(firstPoint, secorndPoint))
             {
                 _ai.grounded = true;
